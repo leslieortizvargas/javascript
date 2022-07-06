cb7294e78093d69d0eb8b5a16d1a9fd14ee6e71d
leslieortizvargas <107438323+leslieortizvargas@users.noreply.github.com>
2022-07-05 20:46:21 -0400

    Update chal2.js

--- chal2.js
+++ chal2.js
@@ -1,6 +1,6 @@
 //EASY: Create two variables and assign a person’s name to each. Console log a statement that says which name is shorter or longer, and by how many characters. Example output: “The name Thomas is longer than John by 2 characters”
-var name1 = "Thomas";
-var name2 = "John";
+var name1 = "Leslie";
+var name2 = "Anna";
 if (name1.length > name2.length) {
 console.log(
 "The name " + name1 + " is longer than " + name2 + " by " + (name1.length - name2.length) + " characters"
